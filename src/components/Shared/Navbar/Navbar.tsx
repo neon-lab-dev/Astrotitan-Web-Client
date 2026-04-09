5388208cb165cc8696ba1f9c7d45f3611fba451d
rahulsd380 <rahulsd380@gmail.com>
2026-04-09 09:41:03 +0600

    Completed navbar

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,12 +1,48 @@
+import { Link } from "react-router-dom";
 import { ICONS } from "../../../assets";
 import Container from "../../Reusable/Container/Container";
+import Button from "../../Reusable/Button/Button";
 
 const Navbar = () => {
+  const navLinks = [
+    {
+      label: "Consult Astrologers",
+      path: "/consult-astrologers",
+    },
+    {
+      label: "How It Works",
+      path: "/how-it-works",
+    },
+    {
+      label: "Insights",
+      path: "/insights",
+    },
+  ];
+
   return (
-    <div>
+    <div className="pt-14 pb-6">
       <Container>
-        <div>
-          <img src={ICONS.astrotitanLogo} alt="" />
+        <div className="flex items-center justify-between">
+          <Link to="/">
+            <img src={ICONS.astrotitanLogo} alt="" />
+          </Link>
+
+          <div className="flex items-center gap-6">
+            {navLinks?.map((item, index) => (
+              <Link
+                key={index}
+                to={item?.path}
+                className="relative text-[21px] leading-7.75 text-neutral-5 pb-1 group"
+              >
+                {item?.label}
+                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-primary-5 transition-all duration-300 group-hover:w-full"></span>
+              </Link>
+            ))}
+
+            <Link to={"/login"}>
+              <Button label="Login" />
+            </Link>
+          </div>
         </div>
       </Container>
     </div>
