35da96950c7dc1a00eed20b27578b5c1c5bad15a
rahulsd380 <rahulsd380@gmail.com>
2026-04-10 10:57:00 +0600

    Complteted contact us page

--- src/pages/ContactUs/ContactUs.tsx
+++ src/pages/ContactUs/ContactUs.tsx
@@ -1,15 +1,22 @@
 import ContactDetails from "../../components/ContactUsPage/ContactDetails/ContactDetails";
 import ContactUsForm from "../../components/ContactUsPage/ContactUsForm/ContactUsForm";
 import ContactUsHero from "../../components/ContactUsPage/ContactUsHero/ContactUsHero";
+import FAQ from "../../components/Shared/FAQ/FAQ";
 
 const ContactUs = () => {
-    return (
-        <div>
-            <ContactUsHero/>
-            <ContactDetails/>
-            <ContactUsForm/>
-        </div>
-    );
+  return (
+    <div>
+      <ContactUsHero />
+      <ContactDetails />
+      <ContactUsForm />
+      <div className="py-14 md:py-28 xl:py-26">
+        <h2 className="heading">
+          Your information is सुरक्षित and will only be used to respond to your query.
+        </h2>
+      </div>
+      <FAQ/>
+    </div>
+  );
 };
 
 export default ContactUs;
